6dcc54043ee7acff67a64e13ec55a15e5623d4ae
Katrina Baker <katrinamourine@gmail.com>
2022-10-22 18:28:48 -0700

    adding functions for JS and tags in HTML

--- assets/script.js
+++ assets/script.js
@@ -16,56 +16,63 @@
 // reuse second fucntion to pull next question
 //after questions are over function to hide question div and show results
 
-//declare HTML elements
+const myQuestions = [
+  {
+    title: "Q1: Commonly used data types DO NOT include:",
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts",
+  },
+  {
+    title:
+      "Q2: String values must be enclosed within ____ when being assigned to variables.",
+    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    correctAnswer: "quotes",
+  },
+  {
+    title: "Q3: The first index of an array is ______.",
+    answers: ["programmer's choice", "5", "1", "0"],
+    correctAnswer: "0",
+  },
+  {
+    title:
+      "Q4: Which event occurs when the user clicks once on an HTML element?",
+    answers: ["onclick", "onmouseover", "dblclick", "keydown"],
+    correctAnswer: "onclick",
+  },
+  {
+    title: "Q5: Which method returns the called string to lowercase",
+    answers: ["toLowerCase()", "toLower()", "caseLower()", "none of the above"],
+    correctAnswer: "caseLower()",
+  },
+];
+
+//declare HTML elements==========================================================
 var timer = document.getElementById("timer");
 var timeEl = document.getElementById("time");
 var intro = document.getElementById("intro");
 var questionBox = document.getElementById("questionBox");
 var question = document.getElementById("question");
 var options = document.getElementById("options");
+var answersEl = document.getElementById("answers");
 var results = document.getElementById("results");
 var finalScore = document.getElementById("finalScore");
 var userInitials = document.getElementById("userInitials");
-
 var startBtn = document.getElementById("startBtn");
 
-//declare questions object array
-var myQuestions = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    answers: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: 2,
-  },
-  {
-    title: "Commonly used data types DO NOT include:",
-    answers: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: 2,
-  },
-  {
-    title: "Commonly used data types DO NOT include:",
-    answers: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: 2,
-  },
-  {
-    title: "Commonly used data types DO NOT include:",
-    answers: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: 2,
-  },
-  {
-    title: "Commonly used data types DO NOT include:",
-    answers: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: 2,
-  },
-];
-//these are variables for time
+//variables for questions============================================================
+var currentQuestion = 0;
+var correctAns = 0;
+var score = 0;
+var questionNumber = 0;
+var ScoreResult;
+var questionIndex;
+var currentTime = 0;
+
 var qIndex = 0;
 var time = myQuestions.length * 15;
 var timerId;
 
-//first item event listener
-startBtn.onclick = startGame;
-
-//function for time
+//functions for timer start/stop =====================================================
 function clock() {
   time--;
   timeEl.textContent = time;
@@ -73,116 +80,86 @@ function clock() {
     gameOver();
   }
 }
-//second event function to hide landing, start timer, unhide 1st div
+
+function startTimer() {
+  timerId = setInterval(clock, 1000);
+  timeEl.textContent = time;
+}
+
+function stopTimer() {
+  clearInterval(timerId);
+}
+
+//functions for hide landing page and display question =============================
 function startGame() {
-
-
-
-    timerId=setInterval(clock, 1000);
-    timeEl.textContent = time;
-    // getQuestion();
-
-
+  intro.style.display = "none";
+  questionBox.style.display = "block";
+  question.style.display = "block";
+  startTimer();
+  getQuestion();
+  hide(intro);
+  show(questionBox);
 }
 
-//pull questions using function to add question text add eventlistener for click
-// reuse second fucntion to pull next question
-//after questions are over function to hide question div and show results
+function getQuestion() {
+  currentQuestion++;
+  if (currentQuestion < myQuestions.length) {
+    answersEl.innerHTML = "";
+    displayQuestion();
+  } else {
+    gameOver();
+  }
 
+  function displayQuestion() {
+    question.textContent = myQuestions[currentQuestion].title;
+    for (i = 0; i < myQuestions[currentQuestion].answers.length; i++) {
+      var posAnswer = myQuestions[currentQuestion].answers[i];
+      var posAnswerBtn = document.createElement("button");
+      // posAnswerBtn.setAttribute("class=forclick")
+      //122 from TA for button click
+      posAnswerBtn.setAttribute("value", posAnswer);
+      posAnswerBtn.textContent = posAnswer;
+      answersEl.appendChild(posAnswerBtn);
+    }
+  }
+}
+
+function isCorrect(correctAnswer) {
+  if (
+    myQuestions[currentQuestion].correctAnswer ==
+    myQuestions[currentQuestion].answers[answersEl]
+  ) {
+    score += 10;
+    console.log("correct!");
+  } else {
+    currentTime += 10;
+    console.log("wrong!");
+  }
+}
+//random functions for hiding and showing elements (landing, questionBox, results)=======
+function hide(element) {
+  element.style.display = "none";
+}
+
+//displays element
+function show(element) {
+  element.style.display = "block";
+}
+
+//function for game over/timer ran out================================================
 function gameOver() {
-    console.log("game over")
+  console.log("game over");
+  stopTimer();
+  questionBox.style.display = "none";
+  results.style.display = "flex";
 }
 
-// function showQuestion(q) {
-//   var titleDiv = document.getElementById("title");
-//   titleDiv.textContent = q.title;
+//event listeners (?)
+startBtn.onclick = startGame;
 
-//   var answers = document.querySelectorAll(".answers");
-
-//   answers.forEach(function (element, index) {
-//     element.textContent = q.answers[index];
-
-//     element.addEventListener("click", function () {
-//       element.textConent = "a";
-//     });
-//   });
-// }
-
-// showQuestion(myQuestions);
-
-// var startButton = document.getElementById("startButton");
-// startButton.addEventListener("click", function () {});
-
-//  {
-//  question: "Arrays in JavaScript can be used to store ______.",
-//  answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-//  correctAnswer: "all of the above",
-//  }
-
-//  ]
-
-// var myQuestions = [
-// 	{
-// 		question: "What is 10/2?",
-// 		answers: {
-// 			a: '3',
-// 			b: '5',
-// 			c: '115'
-// 		},
-// 		correctAnswer: 'b'
-// 	},
-// 	{
-// 		question: "What is 30/3?",
-// 		answers: {
-// 			a: '3',
-// 			b: '5',
-// 			c: '10'
-// 		},
-// 		correctAnswer: 'c'
-// 	}
-// ];
-
-// function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-// funtion showQuestions(questions, quizContainer){
-//     var output = [];
-//     var answers;
-
-//     for(var i=0; i<questions.length; i++)
-
-//     answers = [];
-
-//     // for each available answer to this question...
-// 		for(letter in questions[i].answers){
-
-// 			// ...add an html radio button
-// 			answers.push(
-// 				'<label>'
-// 					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-// 					+ letter + ': '
-// 					+ questions[i].answers[letter]
-// 				+ '</label>'
-// 			);
-// 		}
-
-// 		// add this question and its answers to the output
-// 		output.push(
-// 			'<div class="question">' + questions[i].question + '</div>'
-// 			+ '<div class="answers">' + answers.join('') + '</div>'
-// 		);
-// 	}
-
-// 	// finally combine our output list into one string of html and put it on the page
-// 	quizContainer.innerHTML = output.join('');
-//     function showResults(questions, quizContainer, resultsContainer){
-// 		// code will go here
-// 	}
-
-// 	// show the questions
-// 	showQuestions(questions, quizContainer);
-
-// 	// when user clicks submit, show results
-// 	submitButton.onclick = function(){
-// 		showResults(questions, quizContainer, resultsContainer);
-// 	}
-// }
+answersEl.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    isCorrect(event.target);
+    getQuestion();
+  }
+});
